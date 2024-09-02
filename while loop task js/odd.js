c7feeb6d7e2b407e54049e1a479fb0f20e122154
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-02 08:08:52 +0600

    odd number findout

---
+++ while loop task js/odd.js
@@ -0,0 +1,17 @@
+// let num = 61;
+
+// while (num <= 100) {
+//   if (num % 2 != 0) {
+//     console.log('odd number: ', num);
+//   }
+//   num++;
+// }
+
+let number = 78;
+
+while (number <= 100) {
+    if (number % 2 == 0) {
+      console.log('odd number: ', number);
+    }
+    number++;
+  }
