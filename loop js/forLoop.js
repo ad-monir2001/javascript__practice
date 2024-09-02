966de6873ecb930beac565e8cf2708587a66dc8c
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-02 07:39:19 +0600

    commitment while problem solved

---
+++ loop js/forLoop.js
@@ -0,0 +1,11 @@
+const numbers = [23, 42, 53, 26];
+
+console.log(numbers);
+
+for (i = 0; i < numbers.length; i++) {    // same as below
+  console.log(numbers[i]);
+}
+
+// for (i of numbers) {
+//     console.log(i);
+// }
---
+++ while loop task js/commitment.js
@@ -0,0 +1,8 @@
+let days = 1;
+
+while (days <= 60) {
+  console.log(
+    `I wil invest at least 6 hrs every single day for next ${days} days!`
+  );
+  days++;
+}
