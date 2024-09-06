0d21b7a261bbff2cf2b69fa73427bae0d01601ce
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-06 09:11:15 +0600

    task five odd even checker done

--- function js/task3.js
+++ function js/task3.js
@@ -1,21 +1,21 @@
 let numbers = [2, 4, 3];
 let sum = 0;
 for (x of numbers) {
-    sum += x;
+  sum += x;
 }
-let average = sum / numbers.length
+let average = sum / numbers.length;
 // console.log(sum);
 // console.log(average);
 
 // In function what will we use.
 
 function make_avg(number) {
-    for (a of number) {
-        let sum = 0;
-        sum += a;
-    }
-    let average = sum / number.length;
-    console.log(average);
+  for (a of number) {
+    let sum = 0;
+    sum += a;
+  }
+  let average = sum / number.length;
+  console.log(average);
 }
 
-make_avg([2,4,3])
+make_avg([2, 4, 3]);
---
+++ function js/task5.js
@@ -0,0 +1,9 @@
+function odd__even(x) {
+  if (x % 2 == 0) {
+    console.log('Even');
+  } else {
+    console.log('odd');
+  }
+}
+
+odd__even(5)
