527747b5a37c93581b074cd5e9278c2273ae6453
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-09-07 13:38:31 +0600

    check done

---
+++ coding problem/check.js
@@ -0,0 +1,4 @@
+console.log(Math.ceil(0.95));
+console.log(Math.ceil(4));
+console.log(Math.ceil(-7.004));
+console.log(Math.ceil(7.004));
--- coding problem/inchTofeet.js
+++ coding problem/inchTofeet.js
@@ -1,8 +1,8 @@
 function inchToFeet(inch) {
   let feet = inch / 12;
-  let onlyFeet = Math.round(feet);
+  let onlyFeet = Math.floor(feet);
   let onlyInch = Math.round((feet - onlyFeet) * 12);
   let result = `${onlyFeet}ft ${onlyInch}inch`;
   return result;
 }
-console.log(inchToFeet(75));
+console.log(inchToFeet(86));
