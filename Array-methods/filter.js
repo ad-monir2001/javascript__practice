9fae3af58453b7e31962f44b873f6c7095f3b4fc
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-10-24 08:20:59 +0600

    practice on map , filter

---
+++ Array-methods/filter.js
@@ -0,0 +1,4 @@
+const ages = [34, 23, 38, 35, 24, 13, 14];
+
+const evenAges = ages.filter((age) => age % 2 === 0);
+console.log(evenAges);
---
+++ Array-methods/map.js
@@ -0,0 +1,4 @@
+const ages = [34, 23, 38, 35, 24, 12, 14];
+
+const newAges = ages.map((age) => age * 2);
+console.log(newAges);
